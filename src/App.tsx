ad725a3e6e83b1261fd529d27f31d74194c02e56
Rajan Kumar Sah <rajansah369@gmail.com>
2025-09-02 13:15:53 +0530

    all files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+  <h1>Website is under development</h1>
+  <p><strong>DRS Khabar</strong></p>
+    </div>
+
+  )
+}
+
+export default App
